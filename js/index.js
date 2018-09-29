e75da4abc622700259139059f525f77ee98651b0
zhaochunyu122 <1351059611@qq.com>
2018-09-29 17:32:06 +0800

    zcy6

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ $(function () {
     let enter = $("footer ul .enter");
     let close=$(".close");
     let set=$(".set");
+    let input=$(".search input")
     console.log(enter,close);
     enter.click(function () {
         set.css("display","block");
@@ -9,4 +10,7 @@ $(function () {
     close.click(function () {
         set.css("display","none");
     })
+    input.click(function() {
+        location.href="zcy-search.html";
+    });
 })
--- js/zcy-search.js
+++ js/zcy-search.js
@@ -3,11 +3,9 @@ $(function () {
 
     let kuai=$(".kuai");
     let fail=$(".fail");
-    console.log(kuai);
     kuai.click(function () {
         let content=input.val();
         if(content==""){
-        console.log(content);
           fail.css("display","block");
       }else if(content=="水管"){
         window.open("zcy-sousuo.html");
