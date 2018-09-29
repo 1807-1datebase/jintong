eb1b6ed64dfdd97baa790549cdbb637dd00eaad9
Prosperousa <1347500434@qq.com>
2018-09-29 17:18:31 +0800

    晋彤

--- js/gwc.js
+++ js/gwc.js
@@ -2,48 +2,37 @@
 * @Author: Dell
 * @Date:   2018-09-29 13:33:42
 * @Last Modified by:   Dell
-* @Last Modified time: 2018-09-29 13:36:18
+* @Last Modified time: 2018-09-29 15:47:43
 */
 window.onload=function(){
-let jia=document.querySelector("nav .content ul li:nth-child(1) .wenzi .wu img");
-let jian=document.querySelector("nav .content ul li:nth-child(1) .wenzi .wu img:nth-of-type(2)");
-let num=document.querySelector("nav .content ul li:nth-child(1) .wenzi .wu h6");
-let bot=document.querySelector(".bot");
-let cover=document.querySelector(".mask");
-let jiesuan=document.querySelector(".bot .qisong");
-let heji=document.querySelector(".bot .weixuan h1 span");
-let zong=document.querySelector(".mask .upper .xia h5 span");
-let guan=document.querySelector(".mask .upper .tu p");
+let jia=document.querySelector(".jia");
+let jian=document.querySelector(".jian");
+let num=document.querySelector(".P_middle_right h3");
+let bot=document.querySelector(".P_lb_bottom_left");
+let num2=document.querySelectorAll(".P_lb_bottom_left h3");
+let heji=document.querySelector(".P_dibu_left_top h3");
 let count=0;
-console.log(jia);
+console.log(jia,jian,num,bot);
 jia.onclick=function () {
     count++;
     num.innerHTML=count;
-    jian.style.display="block";
     num.style.display="block";
     bot.style.display="block";
-    heji.innerHTML=count*15;
+    heji.innerHTML=count*60;
     let i=heji.innerHTML;
     // console.log(i);
-    zong.innerHTML=parseInt(i)+5;
+    // zong.innerHTML=parseInt(i)+5;
 }
 jian.onclick=function () {
     count--;
     num.innerHTML=count;
     heji.innerHTML=count*45;
     let j=heji.innerHTML;
-    zong.innerHTML=parseInt(j)+5;
+    // zong.innerHTML=parseInt(j)+5;
     if (count==0){
-        jian.style.display="none";
-        num.style.display="none";
         bot.style.display="none";
     }
 
 }
-jiesuan.onclick=function () {
-    cover.style.display="block";
-}
-guan.onclick=function () {
-    cover.style.display="none";
-}
+
 }
